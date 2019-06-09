1b8e09a433befcefb26b4404cfb8fe599b61e443
Sam <qqqcai@126.com>
2019-06-09 10:09:54 +0800

    初始化2

--- zmeiduo/urls.py
+++ zmeiduo/urls.py
@@ -1,4 +1,4 @@
-"""meiduo_project URL Configuration
+"""zmeiduo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
