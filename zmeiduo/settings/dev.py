2d42a7da94a938919ad8368b0657469838a328d4
Sam <qqqcai@126.com>
2019-06-09 11:39:56 +0800

    日志模块 + 数据库配置

--- zmeiduo/settings/dev.py
+++ zmeiduo/settings/dev.py
@@ -84,7 +84,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             # 补充Jinja2模板引擎环境
-            'environment': 'meiduo_mall.utils.jinja2_env.jinja2_environment',
+            'environment': 'zmeiduo.utils.jinja2_env.jinja2_environment',
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -94,19 +94,19 @@ TEMPLATES = [
 
         },
     },
-    # {
-    #     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    #     'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 此处模版改路径
-    #     'APP_DIRS': True,
-    #     'OPTIONS': {
-    #         'context_processors': [
-    #             'django.template.context_processors.debug',
-    #             'django.template.context_processors.request',
-    #             'django.contrib.auth.context_processors.auth',
-    #             'django.contrib.messages.context_processors.messages',
-    #         ],
-    #     },
-    # },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 此处模版改路径
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 
@@ -121,26 +121,19 @@ WSGI_APPLICATION = 'zmeiduo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        # 'HOST': '192.168.126.128',  # 学校 数据库主机
+        # 'HOST': '192.168.83.129',  # E3V2 数据库ip
+        'HOST': '127.0.0.1',  # 学校windows 数据库ip
+        'PORT': 3306,  # 数据库端口
+        'USER': 'sam',  # 数据库用户名
+        'PASSWORD': '123',  # 数据库用户密码
+        'NAME': 'meiduo',  # 数据库名字
+        # 'OPTIONS': {
+        #      'autocommit': True,  # 是否自动提交？
+        #  },
+    },
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         # 'HOST': '192.168.126.128',  # 学校 数据库主机
-#         # 'HOST': '192.168.83.129',  # E3V2 数据库ip
-#         'HOST': '127.0.0.1',  # 学校windows 数据库ip
-#         'PORT': 3306,  # 数据库端口
-#         'USER': 'root',  # 数据库用户名
-#         'PASSWORD': '123',  # 数据库用户密码
-#         'NAME': 'django_demo',  # 数据库名字
-#         # 'OPTIONS': {
-#         #      'autocommit': True,  # 是否自动提交？
-#         #  },
-#     }
-# }
 
 
 # Password validation
@@ -164,7 +157,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 # LANGUAGE_CODE = 'en-us'
 # TIME_ZONE = 'UTC'
 
@@ -186,25 +178,78 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 项目静态文件路径
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_file")]  # 存放查找静态文件的目录 接收的是list
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "zmeiduo/static_file")]  # 存放查找静态文件的目录 接收的是list
 
 # 上传文件的路径
-MEDIA_ROOT = os.path.join(BASE_DIR, "static_file/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "zmeiduo/static_file/media")
 
 
 
 # 下面是设置redis数据库，将session数据储存到redis数据库的配置
+CACHES = {
+    "default": { # 默认
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": { # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://192.168.126.128:6379/4",
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
-# # 后端缓存
-# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-# # 读取配置
-# SESSION_CACHE_ALIAS = "default"
+# session配置
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# 读取配置,默认是缓存到本机内存条，要改了才变
+SESSION_CACHE_ALIAS = "session"
+
+
+
+
+
+# 日志输出器配置
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志信息显示的格式
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 向终端中输出日志
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/meiduo.log'),  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名为django的日志器
+            'handlers': ['console', 'file'],  # 可以同时向终端与文件中输出日志
+            'propagate': True,  # 是否继续传递日志信息
+            'level': 'INFO',  # 日志器接收的最低日志级别
+        },
+    }
+}
