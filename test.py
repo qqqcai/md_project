2a3b9f8b92a1ed4b2924ceeeeb6db1da1a81c90e
Sam <qqqcai@126.com>
2019-06-11 20:23:56 +0800

    完成 图形+短信验证码 注册

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-import random
+# import random
+#
+# sms_code = '%06d' % random.randint(0, 999999)
+#
+# print(sms_code)
 
-sms_code = '%06d' % random.randint(0, 999999)
 
-print(sms_code)
+
+a = b""
+
+b = a.decode()
+print(b)
---
+++ zmeiduo/celery_tasks/main.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+# 创建celery实例
+celery_app = Celery('meiduo')
+# 加载celery配置
+celery_app.config_from_object('zmeiduo.celery_tasks.config')
+
+# 自动注册celery任务
+celery_app.autodiscover_tasks(['zmeiduo.celery_tasks.sms'])
+
